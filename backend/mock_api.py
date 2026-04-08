78e43751258c6c0a31d77e20e231c3f5529423ff
Kris <krischand757@gmail.com>
2026-04-09 00:23:05 +0545

    Change default port from 5000 to 10000

--- backend/mock_api.py
+++ backend/mock_api.py
@@ -48,5 +48,6 @@ def not_found(e):
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == '__main__':
-       port = int(os.environ.get('PORT', 5000))
-       app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
